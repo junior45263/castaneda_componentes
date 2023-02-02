ad0d8b153305a3e36b10f480a8172d650e088132
junior45263 <bc4363624@gmail.com>
2023-02-02 14:37:31 -0600

    v1

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Maps from './mapas/mapas';
+import Carrusel from './mapas/carrusel';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Carrusel/>
     <Maps/>
   </React.StrictMode>
 );
